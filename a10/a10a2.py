db5fddf4d6643e1d3b5d2d11b9306cb6348c875a
Jack Gaino <jack.gaino@indexexchange.com>
2016-12-10 01:27:58 -0500

    even crazier

--- a10/a10a2.py
+++ a10/a10a2.py
@@ -12,26 +12,23 @@ with open('input.txt', 'r') as input_file:
         else:
             commands.append([data[1], data[5], data[6], data[10], data[11]])
 
+
+def do_command(giver, lowWho, lowID, highWho, highID):
+    for who, id, func in [[lowWho, lowID, min], [highWho, highID, max]]:
+        if who == 'output':
+            outputs[id] += min(robots[giver])
+            robots[giver].remove(min(robots[giver]))
+        else:
+            robots[id].append(min(robots[giver]))
+            robots[giver].remove(min(robots[giver]))
+
 while commands:
     for command in commands:
         if len(robots[command[0]]) == 2:
             if 61 in robots[command[0]] and 17 in robots[command[0]]:
                 print 'Robot', command[0]
                 exit()
-            if command[1] == 'output':
-                outputs[command[2]] += min(robots[command[0]])
-                robots[command[0]].remove(min(robots[command[0]]))
-            else:
-                robots[command[2]].append(min(robots[command[0]]))
-                robots[command[0]].remove(min(robots[command[0]]))
-
-            if command[3] == 'output':
-                outputs[command[4]] += max(robots[command[0]])
-                robots[command[0]].remove(max(robots[command[0]]))
-            else:
-                robots[command[4]].append(max(robots[command[0]]))
-                robots[command[0]].remove(max(robots[command[0]]))
+            do_command(*command)
 
             commands.remove(command)
             break
-
