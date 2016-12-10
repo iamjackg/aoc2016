84baef8f387cea3d3ace93a2ed2dab0182b04f05
Jack Gaino <jack.gaino@indexexchange.com>
2016-12-10 01:10:37 -0500

    Added day 10.

---
+++ a10/a10a2.py
@@ -0,0 +1,46 @@
+from collections import defaultdict
+
+robots = defaultdict(list)
+outputs = defaultdict(int)
+
+with open('input.txt', 'r') as input_file:
+    for line in input_file:
+        data = line.split()
+        if 'value' in line:
+            robots[data[-1]].append(int(data[1]))
+
+print robots
+
+
+commands = []
+
+
+with open('input.txt', 'r') as input_file:
+    for line in input_file:
+        data = line.split()
+        if 'gives' in line:
+            commands.append([data[1], data[5], data[6], data[10], data[11]])
+
+while commands:
+    for command in commands:
+        if len(robots[command[0]]) == 2:
+            if 61 in robots[command[0]] and 17 in robots[command[0]]:
+                print 'All done', command[0]
+                exit()
+            if command[1] == 'output':
+                outputs[command[2]] += min(robots[command[0]])
+                robots[command[0]].remove(min(robots[command[0]]))
+            else:
+                robots[command[2]].append(min(robots[command[0]]))
+                robots[command[0]].remove(min(robots[command[0]]))
+
+            if command[3] == 'output':
+                outputs[command[4]] += max(robots[command[0]])
+                robots[command[0]].remove(max(robots[command[0]]))
+            else:
+                robots[command[4]].append(max(robots[command[0]]))
+                robots[command[0]].remove(max(robots[command[0]]))
+
+            commands.remove(command)
+            break
+
